aaea36f3997e9d3ef3a0c89e1481b851676df7d4
Jordan Hall <jordan@hall05.co.uk>
2015-05-03 18:11:59 +0100

    Windows build script #62

---
+++ builds/windows_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download nw.js (Windows x64)
+wget http://dl.nwjs.io/v0.12.1/nwjs-v0.12.1-win-x64.zip
+unzip nwjs-v0.12.1-win-x64.zip
+rm nwjs-v0.12.1-win-x64.zip
+
+# Rename nw.js directory
+mv nwjs-v0.12.1-win-x64 continuum-editor-windows-x64
+
+# Rename nw.js executable
+mv continuum-editor-windows-x64/nw.exe continuum-editor-windows-x64/continuum-editor.exe
+
+# Sync *.html and *.js
+rsync ../* continuum-editor-windows-x64/ -r --exclude ../build

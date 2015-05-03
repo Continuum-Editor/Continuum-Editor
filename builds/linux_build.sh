2011ac6291e0c1c7f9dbe6e9ef897832da3ec7c9
Jordan Hall <jordan@hall05.co.uk>
2015-05-03 17:58:36 +0100

    Linux build script #62

---
+++ builds/linux_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download nw.js (Linux x64)
+wget http://dl.nwjs.io/v0.12.1/nwjs-v0.12.1-linux-x64.tar.gz
+tar -xvf nwjs-v0.12.1-linux-x64.tar.gz
+rm nwjs-v0.12.1-linux-x64.tar.gz -r
+
+# Rename nw.js directory
+mv nwjs-v0.12.1-linux-x64 continuum-editor-linux-x64
+
+# Rename nw.js executable
+mv continuum-editor-linux-x64/nw continuum-editor-linux-x64/continuum-editor
+
+# Sync *.html and *.js
+rsync ../* continuum-editor-linux-x64/ -r --exclude ../build

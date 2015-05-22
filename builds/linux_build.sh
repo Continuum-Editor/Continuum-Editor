49b099fcf1d3df4b6e5872c2affb5347718ffd5f
DivineOmega <jordan@hall05.co.uk>
2015-05-22 23:25:48 +0100

    Updating build scripts for nw.js v0.12.2

--- builds/linux_build.sh
+++ builds/linux_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Download nw.js (Linux x64)
-wget http://dl.nwjs.io/v0.12.1/nwjs-v0.12.1-linux-x64.tar.gz
-tar -xvf nwjs-v0.12.1-linux-x64.tar.gz
-rm nwjs-v0.12.1-linux-x64.tar.gz
+wget http://dl.nwjs.io/v0.12.2/nwjs-v0.12.2-linux-x64.tar.gz
+tar -xvf nwjs-v0.12.2-linux-x64.tar.gz
+rm nwjs-v0.12.2-linux-x64.tar.gz
 
 # Rename nw.js directory
-mv nwjs-v0.12.1-linux-x64 continuum-editor-linux-x64
+mv nwjs-v0.12.2-linux-x64 continuum-editor-linux-x64
 
 # Rename nw.js executable
 mv continuum-editor-linux-x64/nw continuum-editor-linux-x64/continuum-editor
--- builds/windows_build.sh
+++ builds/windows_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Download nw.js (Windows x64)
-wget http://dl.nwjs.io/v0.12.1/nwjs-v0.12.1-win-x64.zip
-unzip nwjs-v0.12.1-win-x64.zip
-rm nwjs-v0.12.1-win-x64.zip
+wget http://dl.nwjs.io/v0.12.2/nwjs-v0.12.2-win-x64.zip
+unzip nwjs-v0.12.2-win-x64.zip
+rm nwjs-v0.12.2-win-x64.zip
 
 # Rename nw.js directory
-mv nwjs-v0.12.1-win-x64 continuum-editor-windows-x64
+mv nwjs-v0.12.2-win-x64 continuum-editor-windows-x64
 
 # Rename nw.js executable
 mv continuum-editor-windows-x64/nw.exe continuum-editor-windows-x64/continuum-editor.exe

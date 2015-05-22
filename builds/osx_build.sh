305362c5bc7262e12025cd14a3e2c6b06560df52
DivineOmega <jordan@hall05.co.uk>
2015-05-22 23:49:37 +0100

    Initial OS X build script #63 - requires

---
+++ builds/osx_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Download nw.js (OSX x64)
+wget http://dl.nwjs.io/v0.12.2/nwjs-v0.12.2-osx-x64.zip
+unzip nwjs-v0.12.2-osx-x64.zip
+rm nwjs-v0.12.2-osx-x64.zip
+
+# Rename nw.js directory
+mv nwjs-v0.12.2-osx-x64 continuum-editor-osx-x64
+
+# Rename nwjs.app bundle
+mv continuum-editor-osx-x64/nwjs.app continuum-editor-osx-x64/continuum-editor.app
+
+# Create app.nw directory
+mkdir continuum-editor-osx-x64/continuum-editor.app/Contents/Resources/app.nw
+
+# Sync *.html and *.js
+rsync ../* continuum-editor-osx-x64/continuum-editor.app/Contents/Resources/app.nw/ -r --exclude ../build
+
+# Remove left over nw.js
+rm continuum-editor-osx-x64/credits.html
+rm continuum-editor-osx-x64/nwjc

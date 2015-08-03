4fdc2d8331b85e97d20d66d4d8b137ff3212b006
Jordan Hall <jordan.hall@rapidweb.biz>
2015-08-03 15:39:57 +0100

    Git addon - display of staged/unstaged filse and ability to move them between the two states #20

--- addons/git/git.js
+++ addons/git/git.js
@@ -17,15 +17,73 @@ function gitAddon()
 	
 		var rootDir = addonSystem.getDirectoryTreeRoot();
 	
-		var cmd = 'cd '+rootDir+' && git status';
+		var cmd = 'cd '+rootDir+' && git status --porcelain';
 				
 		exec(cmd, function(error, stdout, stderr) 
 		{
 			var html = '';
 			
-			var formattedOutput = stdout.replace(/(?:\r\n|\r|\n)/g, '<br/>');
-
-			html += formattedOutput;
+			var lines = stdout.split('\n');
+			
+			var changesNotStaged = [];
+			var changesStaged = []
+			
+			for (var i = 0; i < lines.length; i++) 
+			{
+			    var line = lines[i];
+			    
+			    var file = line.substr(3);
+			    
+			    if (line[0]=='M')
+			    {
+			        changesStaged.push(file);
+			    }
+			    
+			    if (line[1]=='M')
+			    {
+			        changesNotStaged.push(file);
+			    }
+			}
+			
+			html += '<div id="gitAddon">';
+			
+			html += '<p style="font-weight: bold;">Changes not staged for commit:</p>';
+			
+			if (changesNotStaged.length>0)
+			{
+    			for (var i = 0; i < changesNotStaged.length; i++) 
+    			{
+    			    html += '<div class="stageFile" id="'+changesNotStaged[i]+'">&darr; ';
+    			    html += changesNotStaged[i];
+    			    html += '</div>';
+    			}
+			}
+			else
+			{
+			    html += 'None';
+    			html += '<br/>';
+			}
+			
+			html += '<p style="font-weight: bold;">Changes to be commited:</p>';
+			
+			if (changesStaged.length>0)
+			{
+    			for (var i = 0; i < changesStaged.length; i++) 
+    			{
+    			    html += '<div class="unstageFile" id="'+changesStaged[i]+'">&uarr;<span> ';
+    			    html += changesStaged[i];
+    			    html += '</div>';
+    			}
+			}
+			else
+			{
+			    html += 'None';
+    			html += '<br/>';
+			}
+			
+			html += '</div>';
+			
+			//console.log(stdout);
 			
 			addonSystem.setAddonSidebarContent(html);
 		});
@@ -34,5 +92,37 @@ function gitAddon()
 		setTimeout(function() { me.gitDisplay() }, 1000);
 	}
 }
+
+$(document).on('click', '#gitAddon .stageFile', function()
+{
+	var file = $(this).attr('id');
+	
+	var exec = require('child_process').exec;
+	
+	var rootDir = addonSystem.getDirectoryTreeRoot();
+
+	var cmd = 'cd '+rootDir+' && git add '+file;
+			
+	exec(cmd, function(error, stdout, stderr) 
+	{
+	    
+	});
+});
+
+$(document).on('click', '#gitAddon .unstageFile', function()
+{
+	var file = $(this).attr('id');
+	
+	var exec = require('child_process').exec;
+	
+	var rootDir = addonSystem.getDirectoryTreeRoot();
+
+	var cmd = 'cd '+rootDir+' && git reset HEAD '+file;
+			
+	exec(cmd, function(error, stdout, stderr) 
+	{
+	    
+	});
+});
 
 addonSystem.initialiseAddon(new gitAddon());
